337ce1a284c11280c7ad7d27fe6c77156b77dc9f
epictete <john.laterre@gmail.com>
2020-09-02 14:50:29 +0200

    CRUD-1

--- crud-1/index.php
+++ crud-1/index.php
@@ -7,7 +7,8 @@
     <title>CRUD1</title>
 </head>
 <body>
-    <h2>Clients</h2>
+
+    <h2>Afficher tous les clients de manière structurée</h2>
     <table>
 
 <?php
@@ -22,6 +23,45 @@ $rows = $stmt->fetchAll();
 
 foreach ($rows as $row)
 {
+    if ($row->card)
+    {
+        $card = 'Oui';
+        $cardNumber = "Numéro de carte : " . $row->cardNumber . "<br>";
+    }
+    else{
+        $card = 'Non';
+        $cardNumber = '';
+    }
+
+    echo
+    "<tr>" .
+      "<td>" .
+        "Nom : " . $row->lastName . "<br>" .
+        "Prénom : " . $row->firstName . "<br>" .
+        "Date de naissance : " . $row->birthDate . "<br>" .
+        "Carte de fidélité : " . $card . "<br>" .
+        $cardNumber .
+      "</td>" .
+    "</tr>";
+}
+
+?>
+
+    </table>
+
+    <h2>Clients</h2>
+    <table>
+
+<?php
+
+// CLIENTS
+$sql = 'SELECT * FROM clients WHERE card=?';
+$stmt = $pdo->prepare($sql);
+$stmt->execute([true]);
+$rows = $stmt->fetchAll();
+
+foreach ($rows as $row)
+{
     echo
     "<tr>" .
       "<td>" . $row->id . "</td>" .
@@ -30,6 +70,29 @@ foreach ($rows as $row)
       "<td>" . $row->birthDate . "</td>" .
       "<td>" . $row->card . "</td>" .
       "<td>" . $row->cardNumber . "</td>" .
+    "</tr>";
+}
+
+?>
+
+    </table>
+
+    <h2>Clients dont le nom de famille commence par M triés alphabétiquement</h2>
+    <table>
+<?php
+
+// CLIENTS
+$sql = 'SELECT firstName, lastName FROM clients WHERE lastName LIKE ? ORDER BY lastName ASC';
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['M%']);
+$rows = $stmt->fetchAll();
+
+foreach ($rows as $row)
+{
+    echo
+    "<tr>" .
+      "<td><Prénom: >" . $row->firstName . "</td>" .
+      "<td><Nom: >" . $row->lastName . "</td>" .
     "</tr>";
 }
 
@@ -55,6 +118,29 @@ foreach ($rows as $row)
       "<td>" . $row->id . "</td>" .
       "<td>" . $row->type . "</td>" .
     "</tr>";
+}
+
+?>
+
+    </table>
+
+    <h2>Liste de tous els spectacles triés par ordre alphabétique</h2>
+    <table>
+<?php
+
+// CLIENTS
+$sql = '
+    SELECT title, performer, date, startTime
+    FROM shows
+    ORDER BY title ASC
+';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$rows = $stmt->fetchAll();
+
+foreach ($rows as $row)
+{
+    echo $row->title . " par " . $row->performer . ", le " . $row->date . " à " . $row->startTime . "<br>";
 }
 
 ?>
